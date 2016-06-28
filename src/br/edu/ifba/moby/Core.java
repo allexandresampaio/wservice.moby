f7a10d5f9e215f598ddc2b1bfec629746590bd8a
allexandresampaio <allexandresss@gmail.com>
2016-06-28 12:14:01 -0300

    WS com requisição/retorno em JSON e em texto simples

--- src/br/edu/ifba/moby/Core.java
+++ src/br/edu/ifba/moby/Core.java
@@ -1,11 +1,14 @@
 package br.edu.ifba.moby;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
-import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+
+import org.codehaus.jettison.json.JSONException;
+import org.codehaus.jettison.json.JSONObject;
 		
 @Path("ws")
 public class Core {
@@ -17,6 +20,7 @@ public class Core {
 		return "OK";
 	}
 	
+	//Pega pela URL e cria um JSON
 	@GET//era post, mudei para GET e funcionou.
 	@Path("/direcionamento/{id}/{localAtual}/{posicaoRelativa}")
 	@Produces(MediaType.APPLICATION_JSON)
@@ -28,6 +32,22 @@ public class Core {
 		Direcionamento direcao = new Direcionamento(id, localAtual, posicaoRelativa);
 		direcao.calcularDirecao();		
 		return direcao;	
+	}
+	
+	//Pega por um JSON e devolve a direção
+	@GET//era post, mudei para GET e funcionou.
+	@Path("/direcionamento/")
+	@Produces(MediaType.TEXT_PLAIN)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public String solicitarDirecao(JSONObject inputJsonObj) throws JSONException {
+		
+		String id = inputJsonObj.getString("id");
+		String localAtual = inputJsonObj.getString("localAtual");
+		String posicaoRelativa = inputJsonObj.getString("posicaoRelativa");
+		
+		Direcionamento direcao = new Direcionamento(id, localAtual, posicaoRelativa);
+		direcao.calcularDirecao();		
+		return direcao.getProximaDirecao();	
 	}
 	
 	@GET
